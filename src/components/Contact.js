c22428a6efcaa64f48087236902cd7ae821b1c29
NicoBellick <nicolas.soumier@gmail.com>
2021-01-25 06:05:31 +0100

    End of quest

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,51 @@
+// import { ReactComponent } from '*.svg'
+import { render } from '@testing-library/react'
 import React from 'react'
 import './Contact.css'
 import ContactList from './ContactList'
 
 
 
-
-const Contact = (props) => {
+class Contact extends React.Component{
+    constructor(props){
+    super(props);
+    this.state = {
+        online: props.online
+    }}
+    render(){
     return(
-            <div className='Contact'>
-                <img src={props.avatar} alt='RandomPicture' className='avatar' />
+        <div className='Contact'>
+                <img src={this.props.avatar} alt='RandomPicture' className='avatar' />
                 <div>
-                <p className='name'>{props.name}</p>
-                <div className='status'>
+                <p className='name'>{this.props.name}</p>
+                <div className='status' onClick={event =>{
+                    const newStatus = !this.state.online;
+                    this.setState({online: newStatus})
+                }}>
                     
-                <div className={props.online ? 'status-online' : 'status-offline'}></div>
-                <p className='status-text'>{props.online ? "Available" : "Offline"}</p>
+                <div className={this.state.online ? 'status-online' : 'status-offline'} ></div>
+                <p className='status-text'>{this.state.online ? "Available" : "Offline"}</p>
                 </div>
                 </div>
             </div>
     )
-}
+    
+    }}
+// const Contact = (props) => {
+//     return(
+//             <div className='Contact'>
+//                 <img src={props.avatar} alt='RandomPicture' className='avatar' />
+//                 <div>
+//                 <p className='name'>{props.name}</p>
+//                 <div className='status'>
+                    
+//                 <div className={props.online ? 'status-online' : 'status-offline'}></div>
+//                 <p className='status-text'>{props.online ? "Available" : "Offline"}</p>
+//                 </div>
+//                 </div>
+//             </div>
+//     )
+// }
 
 //
 
